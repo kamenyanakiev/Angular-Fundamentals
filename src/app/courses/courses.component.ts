a48fdb96a976266ffea3f670de329538fb9f993b
kamen-yanakiev <kamenqnakiev@gmail.com>
2020-10-30 11:43:28 +0200

    components done

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+  selectedCourse = null;
   courses = [
     {
       id: 1,
@@ -13,12 +14,23 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'Angular 9 test',
+      description: 'Learn the fundamentals of Angular 9 test',
+      percentComplete: 50,
+      favorite: true
     }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectCourse(course) {
+    this.selectedCourse = course;
   }
 
 }
